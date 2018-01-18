d6bb9cd7c665066cc32e29d2c1d88cdb97e37248
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-01-18 15:51:19 +0100

    add script to download cromwell

---
+++ download_cromwell.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+wget -c https://github.com/broadinstitute/cromwell/releases/download/30.1/cromwell-30.1.jar
+wget -c https://github.com/broadinstitute/cromwell/releases/download/30.1/womtool-30.1.jar
