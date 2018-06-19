dda1a7e69a808b611dc1d426e7cbf7bd024f93c5
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-06-19 13:35:49 +0200

    add some seqstats tests

--- src/test/scala/biowdl/test/QCFilesPresent.scala
+++ src/test/scala/biowdl/test/QCFilesPresent.scala
@@ -21,7 +21,10 @@
 
 package biowdl.test
 
+import java.io.File
+
 import nl.biopet.utils.biowdl.PipelineSuccess
+
 import scala.util.matching.Regex
 
 trait QCFilesPresent extends QC with PipelineSuccess {
@@ -86,4 +89,56 @@ trait QCFilesPresent extends QC with PipelineSuccess {
         mustHaveFastqcDir(
           s"QCafter/read2/fastqc/${"cutadapt_" + fastqcName(file.getName)}"))
   }
+
+  val seqstatRead1: File = new File(outputDir, "QC/read1/seqstat.json")
+  val seqstatRead2: Option[File] =
+    if (read2.isDefined) Some(new File(outputDir, "QC/read2/seqstat.json"))
+    else None
+  val seqstatRead1AfterClipping: Option[File] =
+    if (adapterClippingRuns)
+      Some(new File(outputDir, "QCafter/read1/seqstat.json"))
+    else None
+  val seqstatRead2AfterClipping: Option[File] =
+    if (adapterClippingRuns && read2.isDefined)
+      Some(new File(outputDir, "QCafter/read1/seqstat.json"))
+    else None
+
+  val adaptersRead1: File =
+    new File(outputDir, "QC/read1/extractAdapters/adapter.list")
+  val adaptersRead2: Option[File] =
+    if (read2.isDefined)
+      Some(new File(outputDir, "QC/read1/extractAdapters/adapter.list"))
+    else None
+
+  val contaminationsRead1: File =
+    new File(outputDir, "QC/read1/extractAdapters/contaminations.list")
+  val contaminationsRead2: Option[File] =
+    if (read2.isDefined)
+      Some(new File(outputDir, "QC/read2/extractAdapters/contaminations.list"))
+    else None
+
+  val fastqcRead1: File = new File(
+    outputDir,
+    s"QC/read1/fastqc/${fastqcName(read1.getName)}/fastqc_data.txt")
+  val fastqcRead2: Option[File] =
+    if (read2.isDefined)
+      Some(
+        new File(
+          outputDir,
+          s"QC/read1/fastqc/${fastqcName(read2.map(_.getName).getOrElse(""))}/fastqc_data.txt"))
+    else None
+  val fastqcRead1AfterClipping: Option[File] =
+    if (adapterClippingRuns)
+      Some(
+        new File(
+          outputDir,
+          s"QCafter/read1/fastqc/${fastqcName(read1.getName)}/fastqc_data.txt"))
+    else None
+  val fastqcRead2AfterClipping: Option[File] =
+    if (read2.isDefined && adapterClippingRuns)
+      Some(
+        new File(
+          outputDir,
+          s"QC/read2/fastqc/${fastqcName(read2.map(_.getName).getOrElse(""))}/fastqc_data.txt"))
+    else None
 }
--- src/test/scala/biowdl/test/QCSuccess.scala
+++ src/test/scala/biowdl/test/QCSuccess.scala
@@ -1,34 +1,22 @@
 package biowdl.test
 
-import nl.biopet.utils.biowdl.PipelineSuccess
-import nl.biopet.utils.conversions.fileToJson
-import play.api.libs.json._
-import java.io.File
+import nl.biopet.tools.seqstat.schema.Root
 
-trait QCSuccess extends QCFilesPresent {
+import nl.biopet.test.BiopetTest
+import org.testng.annotations.Test
 
-  val seqstatRead1: File = new File(outputDir,"QC/read1/seqstat.json")
-  val seqstatRead2: Option[File] = if (read2.isDefined) Some(new File(outputDir, "QC/read2/seqstat.json")) else None
-  val seqstatRead1AfterClipping: Option[File] = if (adapterClippingRuns) Some(new File(outputDir,"QCafter/read1/seqstat.json")) else None
-  val seqstatRead2AfterClipping: Option[File] = if (adapterClippingRuns && read2.isDefined) Some(new File(outputDir,"QCafter/read1/seqstat.json")) else None
-  
-  val adaptersRead1: File = new File(outputDir,"QC/read1/extractAdapters/adapter.list")
-  val adaptersRead2: Option[File] = if (read2.isDefined) Some(new File(outputDir,"QC/read1/extractAdapters/adapter.list")) else None
+trait QCSuccess extends QCFilesPresent with BiopetTest {
 
-  val contaminationsRead1: File = new File(outputDir,"QC/read1/extractAdapters/contaminations.list")
-  val contaminationsRead2: Option[File] = if (read2.isDefined) Some(new File(outputDir,"QC/read2/extractAdapters/contaminations.list")) else None
-
-  val fastqcRead1: File = new File (outputDir, s"QC/read1/fastqc/${fastqcName(read1.getName)}/fastqc_data.txt")
-  val fastqcRead2: Option[File] = if (read2.isDefined) Some(new File (outputDir, s"QC/read1/fastqc/${fastqcName(read2.map(_.getName).getOrElse(""))}/fastqc_data.txt")) else None
-  val fastqcRead1AfterClipping: Option[File] = if (adapterClippingRuns) Some(new File (outputDir, s"QCafter/read1/fastqc/${fastqcName(read1.getName)}/fastqc_data.txt")) else None
-  val fastqcRead2AfterClipping: Option[File] = if (read2.isDefined && adapterClippingRuns) Some(new File (outputDir, s"QC/read2/fastqc/${fastqcName(read2.map(_.getName).getOrElse(""))}/fastqc_data.txt")) else None
-}
-
-trait QCvalues extends QCSuccess {
-  def testSeqStatsRead1 : Unit ={
-    val seqstatJson = fileToJson(seqstatRead1)
-    seqstatJson
-
+  @Test
+  def testSeqStatsRead1: Unit = {
+    val seqstats: Root = Root.fromFile(seqstatRead1)
+    seqstats.seqstat.foreach(x => {
+      val reads = x.r1
+      reads.readsTotal shouldBe 1000
+      reads.minLength shouldBe 100
+      reads.maxLength shouldBe 100
+      reads.qualityEncoding shouldBe List("sanger")
+    })
   }
 
 }
