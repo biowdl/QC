ed19ab9d24a79aee0919543f7f664fd4b85ef848
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-08-24 08:56:14 +0200

    more accurate naming of constants

--- src/test/scala/biowdl/test/QCSuccess.scala
+++ src/test/scala/biowdl/test/QCSuccess.scala
@@ -36,8 +36,10 @@ trait QCSuccess extends QCFilesPresent with BiopetTest {
   val read2MaxLength = 100
   val read1MinLength = 100
   val read2MinLength = 100
-  val totalReads = 1000
-  val totalBases = 100000
+  val read1TotalReads = 1000
+  val read1TotalBases = 100000
+  val read2TotalReads = 1000
+  val read2TotalBases = 100000
   val encodings = List("Illumina 1.8+")
 
   @Test
@@ -50,19 +52,19 @@ trait QCSuccess extends QCFilesPresent with BiopetTest {
       .seqstat
     seqstat.r1.aggregation.maxLength shouldBe read1MaxLength
     seqstat.r1.aggregation.minLength shouldBe read1MinLength
-    seqstat.r1.aggregation.readsTotal shouldBe totalReads
+    seqstat.r1.aggregation.readsTotal shouldBe read1TotalReads
     seqstat.r1.aggregation.qualityEncoding shouldBe encodings
 
     seqstat.r2.foreach { read =>
       read.aggregation.minLength shouldBe read2MinLength
       read.aggregation.maxLength shouldBe read2MaxLength
-      read.aggregation.readsTotal shouldBe totalReads
+      read.aggregation.readsTotal shouldBe read2TotalReads
       read.aggregation.qualityEncoding shouldBe encodings
     }
 
     val groupStats = seqstat.asGroupStats
-    groupStats.r1qual.totalBases shouldBe totalBases
-    groupStats.r2qual.foreach(_.totalBases shouldBe totalBases)
+    groupStats.r1qual.totalBases shouldBe read1TotalBases
+    groupStats.r2qual.foreach(_.totalBases shouldBe read2TotalBases)
     groupStats.isPaired shouldBe this.read2.isDefined
   }
 
@@ -76,19 +78,19 @@ trait QCSuccess extends QCFilesPresent with BiopetTest {
         .libraries("library")
         .readgroups("readgroup")
         .seqstat
-      seqstat.r1.aggregation.readsTotal shouldNot be(totalReads) // Some reads should be dropped after clipping
+      seqstat.r1.aggregation.readsTotal shouldNot be(read1TotalReads) // Some reads should be dropped after clipping
       seqstat.r1.aggregation.maxLength shouldBe read1MaxLength // Assuming some reads are not cut
       seqstat.r1.aggregation.minLength shouldNot be(read1MinLength) // some reads should be cut
-      seqstat.r1.aggregation.basesTotal shouldNot be(totalBases) // Definitely some bases will have been gone.
+      seqstat.r1.aggregation.basesTotal shouldNot be(read1TotalBases) // Definitely some bases will have been gone.
       seqstat.r2.foreach { read =>
-        read.aggregation.readsTotal shouldNot be(totalReads)
+        read.aggregation.readsTotal shouldNot be(read2TotalReads)
         read.aggregation.maxLength shouldBe read2MaxLength
         read.aggregation.minLength shouldNot be(read2MinLength)
-        read.aggregation.basesTotal shouldNot be(totalBases)
+        read.aggregation.basesTotal shouldNot be(read2TotalBases)
       }
       val groupStats: GroupStats = seqstat.asGroupStats
-      groupStats.r1qual.totalBases shouldNot be(totalBases)
-      groupStats.r2qual.foreach(_.totalBases shouldNot be(totalBases))
+      groupStats.r1qual.totalBases shouldNot be(read1TotalBases)
+      groupStats.r2qual.foreach(_.totalBases shouldNot be(read1TotalBases))
       groupStats.isPaired shouldBe this.read2.isDefined
     }
   }
